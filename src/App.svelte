af2138cfdb74448e72358597d9b98e98ec114e51
Philipp <p.pichler1@gmail.com>
2021-03-19 22:24:57 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let count = 0;
+    function sum() {
+        chrome.tabs.executeScript({
+            code: 'console.log(document.body)'
+        });
+        count++;
+    }
+</script>
+
+<p>Counter: {count}</p>
+<button on:click={sum}> + </button>
+
+<style>
+    body {
+        background: red;
+    }
+</style>
