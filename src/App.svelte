6a96817dfe2475c06a15eba0416f6b4b5c61a9e0
Philipp <p.pichler1@gmail.com>
2021-03-19 23:15:50 +0100

    adding icon and readme and adding style

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,41 @@
 <script lang="ts">
-    let count = 0;
-    function sum() {
-        chrome.tabs.executeScript({
-            code: 'console.log(document.body)'
-        });
-        count++;
-    }
+    let currentUrl = '';
+
+    (chrome as any).browserAction?.setIcon({ path: "favicon-96x96.png" });
+    (chrome as any).tabs?.query({active: true, lastFocusedWindow: true}, tabs => {
+        currentUrl = tabs[0].url;
+    });
+
 </script>
 
-<p>Counter: {count}</p>
-<button on:click={sum}> + </button>
-
+<div class="extension">
+<div class="heading">Meta-Mapper.com Extension</div>
+<div>
+<a target="_blank" href="https://meta-mapper.com/editor?adapter=meta_data&data={currentUrl}">Open this Page!</a>
+</div>
+</div>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
     body {
-        background: red;
+
+    }
+    .extension {
+        background: linear-gradient(200deg,#fdc8f2, #26ffe6);
+        margin: -10px;
+        padding: 10px;
+        font-family: 'Open Sans', sans-serif;
+    }
+    .heading {
+        min-width: 300px !important;
+        font-size: 24px;
+        font-family: 'Archivo Black', sans-serif;
+        margin-bottom: 15px;
+    }
+
+    a {
+        font-size: 24px;
+        color: black;
+        font-weight: bold;
     }
 </style>
